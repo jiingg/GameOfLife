233c9c00abecc98427555999206636fee9ed57db
jiingg <yanji.bb@gmail.com>
2020-04-29 16:27:10 -0400

    Life goes by stage

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,68 @@
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map startMap = new Map();
 
-        startMap.setMap(sc.nextInt(), sc.nextInt(), true);
+        Map universe = new Map();
+        int size = sc.nextInt();
+        //long seed = sc.nextLong();
+        int gen = 10;
+        char[][] myMap;
+        myMap = universe.startMap(size);
+
+        if (gen < 1) {
+            System.out.println("Generation #" + gen);
+            for (char[] chars : myMap) {
+                for (int j = 0; j < myMap.length; j++) {
+                    System.out.print(chars[j]);
+                }
+                System.out.println();
+            }
+        } else {
+            for (int i = 0; i < gen; i++) {
+
+                myMap = universe.afterMap(myMap);
+                int lives = 0;
+
+                for (char[] chars : myMap) {
+                    for (int j = 0; j < myMap.length; j++) {
+                        if (chars[j] == 'O') {
+                            lives++;
+                        }
+                    }
+                    System.out.println();
+                }
+                System.out.println("Generation #" + (i+1));
+                System.out.println("Alive: " + lives);
+
+                for (char[] chars : myMap) {
+                    for (int j = 0; j < myMap.length; j++) {
+                        System.out.print(chars[j]);
+                    }
+                    System.out.println();
+                }
+
+                try {
+                    if (System.getProperty("os.name").contains("Windows")) {
+                        Thread.sleep(1000);
+                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+                    }
+                    else {
+
+                        Runtime.getRuntime().exec("clear");
+                    }
+                }
+                catch (IOException | InterruptedException e) {}
+                System.out.println();
+            }
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -1,26 +1,116 @@
 import java.util.Random;
 
 public class Map {
-    private int size;
-    private int seed;
-    private boolean life;
 
-    public void setMap (int size, int seed, boolean life) {
-        this.size = size;
-        this.seed = seed;
-        this.life = life;
+    public char[][] startMap(int startSize) {
+        boolean life;
+        char[][] map = new char[startSize][startSize];
 
-        Random random = new Random(seed);
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
+        Random random = new Random();
+        for (int i = 0; i < startSize; i++) {
+            for (int j = 0; j < startSize; j++) {
                 life = random.nextBoolean();
                 if (life) {
-                    System.out.print('O');
+                    map[i][j] = 'O';
+                    //System.out.print(map[i][j]);
                 } else {
-                    System.out.print(" ");
+                    map[i][j] = 'X';
+                    //System.out.print(map[i][j]);
                 }
             }
-            System.out.println();
+            //System.out.println();
         }
+        return map;
+    }
+
+    public char[][] afterMap(char[][] startMap) {
+        int count = 0;
+        int leftI ;
+        int rightI;
+        int leftJ;
+        int rightJ;
+        char[][] afterMap = new char[startMap.length][startMap.length];
+
+
+        for (int i = 0; i < startMap.length; i++) {
+            for (int j = 0; j < startMap.length; j++) {
+                if (i == 0) {
+                        leftI = startMap.length - 1;
+                        rightI = i + 1;
+                    } else if (i == startMap.length - 1) {
+                        rightI = 0;
+                        leftI = i - 1;
+                    } else {
+                        rightI = i + 1;
+                        leftI = i - 1;
+                    }
+                if (j == 0) {
+                        leftJ = startMap.length - 1;
+                        rightJ = j + 1;
+                    } else if (j == startMap.length - 1) {
+                        rightJ = 0;
+                        leftJ = j - 1;
+                    } else {
+                        rightJ = j + 1;
+                        leftJ = j - 1;
+                    }
+                    //1
+                if (startMap[leftI][leftJ] == 'O') {
+                        count++;
+                    }
+                    //2
+                if (startMap[leftI][j] == 'O') {
+                        count++;
+                    }
+                    //3
+                if (startMap[leftI][rightJ] == 'O') {
+                        count++;
+                    }
+                    //4
+                if (startMap[i][leftJ] == 'O') {
+                        count++;
+                    }
+                    //5
+                if (startMap[i][rightJ] == 'O') {
+                        count++;
+                    }
+                    //6
+                if (startMap[rightI][leftJ] == 'O') {
+                        count++;
+                    }
+                    //7
+                if (startMap[rightI][j] == 'O') {
+                        count++;
+                    }
+                    //8
+                if (startMap[rightI][rightJ] == 'O') {
+                        count++;
+                    }
+
+                if (startMap[i][j] == 'O') {
+                    if (count == 2 || count == 3) {
+                            afterMap[i][j] = 'O';
+                            //System.out.print(afterMap[i][j]);
+                    } else {
+                            afterMap[i][j] = ' ';
+                            //System.out.print(afterMap[i][j]);
+                    }
+
+                } else if (startMap[i][j] == ' ') {
+                    if (count == 3) {
+                            afterMap[i][j] = 'O';
+                            //System.out.print(afterMap[i][j]);
+                    } else {
+                            afterMap[i][j] = ' ';
+                            //System.out.print(afterMap[i][j]);
+                    }
+
+                }
+                    count = 0;
+            }
+            //System.out.println();
+        }
+
+        return afterMap;
     }
 }
